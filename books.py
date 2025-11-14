c309df66ee8703f2da0467c9fc6b827d641630c5
edzen12 <oichiev.edzen@gmail.com>
2025-11-14 10:17:04 +0600

    сделал половину books

--- books.py
+++ books.py
@@ -1,26 +1,54 @@
-ЗАДАЧА: Cистема книжного магазина
-Требуется реализовать систему управления книжным магазином с использованием инкапсуляции, наследования, аргументов *args и **kwargs.
-класс BaseBook:
-защищённые атрибуты: _title, _author
-приватный атрибут: __price
-свойство price с проверкой (цена ≥ 100)
-абстрактный метод info()
-Классы-наследники от BaseBook:
-• Book — обычная книга. Реализация info(): «Книга: <title> — <author>, <price> сом»
-• EBook — электронная книга. Доп. атрибут: _file_size_mb. info(): «Электронная книга: <title> — <author>, <price> сом, файл <size> МБ»
-• AudioBook — аудиокнига. Доп. атрибут: _duration_min. info(): «Аудиокнига: <title> — <author>, <price> сом, длительность <minutes> мин»
-Класс Inventory (склад):
-защищённый список _books
-метод add_books(*books): принимает любое количество объектов книг, проверяет тип
-метод find_books(**filters): возвращает список книг, соответствующих переданным параметрам
-метод remove_book(book): удаляет книгу
-метод all_books(): возвращает копию списка книг
-Класс BookStore:
-атрибут name
-объект inventory
-приватный атрибут __income + свойство income (только чтение)
-метод sell_book(title): ищет по названию, удаляет книгу, увеличивает доход
-метод show_status(): возвращает название магазина, доход и список всех книг через info()
-Требования:
-обязательно использовать инкапсуляцию (__ и _), наследование, полиморфизм, *args, **kwargs
-система должна демонстрировать добавление книг, поиск, продажу и отображение состояния магазина
+class BaseBook:
+    def __init__(self, title, author, price):
+        self._title = title
+        self._author = author
+        self.__price = price
+    
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if value>= 100:
+            self.__price = value
+
+    def info(self):
+        return f"{self._title} {self._author} {self.price}"
+    
+class Book(BaseBook):
+    def info(self):
+        return f"Книга: {self._title} {self._author} {self.price}сом»" 
+
+class EBook(BaseBook):
+    def __init__(self, title, author, price, file_size_mb):
+        super().__init__(title, author, price)
+        self._file_size_mb = file_size_mb
+
+    def info(self): 
+        return f"Электронная книга: {self._title} — {self._author}, {self.price} сом, файл {self._file_size_mb} МБ"
+    
+class AudioBook(BaseBook):
+    def __init__(self, title, author, price, duration_min):
+        super().__init__(title, author, price)
+        self._duration_min = duration_min
+
+    def info(self): 
+        return f"Электронная книга: {self._title} — {self._author}, {self.price} сом, длительность {self._duration_min} мин»"
+    
+# Класс Inventory (склад):
+# защищённый список _books
+# метод add_books(*books): принимает любое количество объектов книг, проверяет тип
+# метод find_books(**filters): возвращает список книг, соответствующих переданным параметрам
+# метод remove_book(book): удаляет книгу
+# метод all_books(): возвращает копию списка книг
+# Класс BookStore:
+# атрибут name
+# объект inventory
+# приватный атрибут __income + свойство income (только чтение)
+# метод sell_book(title): ищет по названию, удаляет книгу, увеличивает доход
+# метод show_status(): возвращает название магазина, доход и список всех книг через info()
+# Требования:
+# обязательно использовать инкапсуляцию (__ и _), наследование, полиморфизм, *args, **kwargs
+# система должна демонстрировать добавление книг, поиск, продажу и отображение состояния магазина
+
